c770a830a45e7803f16aab810b5340332bd8cb1f
tolaDiego <dtolaquispe@frba.utn.edu.ar>
2023-11-26 13:10:52 -0300

    populate andando

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -3,15 +3,15 @@ const Doctors = require('../models/doctorModel')
 const doctorController = {
     getAllDoctors: async (req, res) => {
         try {
-            const foundDoctors = await Doctors.find().populate("plan")
+            const foundDoctors = await Doctors.find().populate("plan",{cost:1})
             return res.status(200).json({ sucess: true, message: "All doctors", doctor: foundDoctors })
         } catch (error) {
-            return res.status(500).json({ sucess: false })
+            return res.status(500).json({ sucess: false ,message:"error request doctors"})
         }
     },
     getDoctor: async (req, res) => {
         try {
-            const foundDoctor = await Doctors.findOne({ _id: req.params.id })
+            const foundDoctor = await Doctors.findOne({ _id: req.params.id }).polulate("planes")
             return res.status(200).json({ sucess: true, doctor: foundDoctor, message: "The doctor has been found" })
         } catch (error) {
             return res.status(500).json({ sucess: false })
--- models/doctorModel.js
+++ models/doctorModel.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose')
 
+const planSchema = new mongoose.Schema({
+    planName: {type:String},
+    description: {type:String},
+    coverage: [{type:String}],
+    cost: {type:Number}
+  });
+  mongoose.model('planes', planSchema)
 const doctorsSchema = new mongoose.Schema({
     name : {type:String, required:true},
     lastName : {type:String, required:true},
@@ -9,9 +16,8 @@ const doctorsSchema = new mongoose.Schema({
     dni : {type:Number, required:true},
     license : {type:Number, required:true},
     meetCost : {type:Number, required:true},
-    idPlan:{type:mongoose.Types.ObjectId, ref:"planes"}
+    plan:{ type: mongoose.Types.ObjectId, ref: 'planes'}
 })
 
 const Doctors = mongoose.model('doctors', doctorsSchema)
-
 module.exports = Doctors
